88797e3b6cd2df2d54a9fdaee5f8363a17de0130
sb <lauren026@naver.com>
2017-07-27 15:26:02 +0900

    7단 커밋

--- gugudan.c
+++ gugudan.c
@@ -12,7 +12,8 @@ void gugudan_2();
 void gugudan_3();
 void gugudan_4();
 void gugudan_5();
-
+void gugudan_6();
+void gugudan_7();
 
 
 
@@ -26,6 +27,7 @@ int main() {
 	if (choice == 1) {
 		gugudan();
 	}
+
 
 	else
 		printf("input error \n");
@@ -45,6 +47,9 @@ void gugudan() {
 	case 3:gugudan_3(); break;
 	case 4:gugudan_4(); break;
 	case 5:gugudan_5(); break;
+	case 6:gugudan_6(); break;
+	case 7:gugudan_7(); break;
+
 	}
 }
 
@@ -96,6 +101,28 @@ void gugudan_5() {
 	int i;
 	int j;
 	for (i = 1; i < 6; i++) {
+		for (j = 1; j < 10; j++) {
+			printf("%d x %d = %d\n", i, j, i*j);
+		}
+		printf("\n");
+	}
+}
+
+void gugudan_6() {
+	int i;
+	int j;
+	for (i = 1; i < 7; i++) {
+		for (j = 1; j < 10; j++) {
+			printf("%d x %d = %d\n", i, j, i*j);
+		}
+		printf("\n");
+	}
+}
+
+void gugudan_7() {
+	int i;
+	int j;
+	for (i = 1; i < 8; i++) {
 		for (j = 1; j < 10; j++) {
 			printf("%d x %d = %d\n", i, j, i*j);
 		}
